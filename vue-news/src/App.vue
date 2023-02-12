2bc1f7b72ff8754eb878d8f9dbfeebbe3de0abcd
KUEUNJI <yoo128@naver.com>
2023-02-12 19:22:35 +0900

    ⭐라우터 기본

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    hello
+    <tool-bar></tool-bar>
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue'
+
 export default {
   name: "App",
+  components: {
+    ToolBar
+  }
 };
 </script>
 
